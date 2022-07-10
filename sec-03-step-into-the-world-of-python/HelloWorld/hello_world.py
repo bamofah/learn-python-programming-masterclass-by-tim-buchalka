7dc9ee875dcf6ce5c79cb3039af918535ff2c675
berna <thewest1989@gmail.com>
2022-07-10 03:41:39 +0100

    Sec 03 Stepping in the World of Python -> Strings in Python

--- sec-03-step-into-the-world-of-python/HelloWorld/hello_world.py
+++ sec-03-step-into-the-world-of-python/HelloWorld/hello_world.py
@@ -2,3 +2,7 @@ print("Hello World!")
 
 name = 'Bernard'
 print(f'Hello {name}!')
+print(1 + 2)
+print(7 * 6)
+print()
+print('The end')
---
+++ sec-03-step-into-the-world-of-python/HelloWorld/strings.py
@@ -0,0 +1,15 @@
+print("Today is a good day to leanr Python")
+print('Python is fun')
+print("Python's string are easy to use")
+print('We can even include "quotes" in strings')
+print("Hello " + "World")
+greeting = "Hello"
+name = "Bruce"
+
+# if we want a space we can add that too
+print(greeting + ' ' + name)
+
+# get value from user input (keyboard/terminal)
+name = input('Please enter your name ')
+
+print(greeting + ' ' + name)
